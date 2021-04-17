a0dfce948ffe31235a8506afe992f4d2f8133a37
SpaceClouds42 <spaceclouds42@gmail.com>
2021-04-17 10:57:37 -0400

    getting of mc version and meta files

--- merge.sh
+++ merge.sh
@@ -1,11 +1,71 @@
 #!/bin/bash
 
+# $1 is the exit code
+# Sends a help message and ends script
 function sendHelp {
-	echo "Minecraft Jar Getter is a simple™ tool for downloading and merging the Minecraft client and server jars"
-	echo "Use ./merge <version> to get the merged jar for the desired version"
-	echo "Example: \"./merge 21w15a\" will output minecraft-21w15a-merged.jar"
+	echo "Need help?"
+	echo "./merge.sh <version> to get the merged jar for the desired version"
+	echo "./merge.sh (-r|-s)"
+	echo "  -r: latest release"
+	echo "  -s: latest snapshot"
+	exit $1
 }
 
-if [[ $1 == "" ]]; then
-	sendHelp
+# $1 is version type argument (release or snapshot)
+# Gets the latest release/snapshot from mojang launchermeta
+# returns the latest version
+function getLatestVer {
+	if [[ $1 == -r ]]; then
+		latest=$(curl https://launchermeta.mojang.com/mc/game/version_manifest.json -L | grep -Po "\"release\": \"\K.*?(?=\")")
+	elif [[ $1 == -s ]]; then
+		latest=$(curl https://launchermeta.mojang.com/mc/game/version_manifest.json -L | grep -Po "\"snapshot\": \"\K.*?(?=\")")
+	fi
+	echo $latest
+}
+
+# $1 is the version
+# Gets the version meta for version $1
+function getVerMeta {
+	echo $(curl https://launchermeta.mojang.com/mc/game/version_manifest.json -L | grep -Po "id\": \"$1\".+?l\": \"\K.+?$1\.json")
+}
+
+# $1 is the version meta
+# Downloads the client jar to /tmp/client-$1.jar
+function downloadClient {
+	echo "Totally downloading client.jar"
+}
+
+# $1 is the version meta
+# Downloads the server jar to /tmp/server-$1.jar
+function downloadServer {
+	echo "Totally downloading server.jar"
+}
+
+mcVer=""
+##########################
+#                        #
+# Merge.sh Input Handler #
+#                        #
+##########################
+# No arg or help arg
+if [[ $1 == "" || $1 == -h || $1 == --help ]]; then
+	sendHelp 0
+
+# Latest release or snapshot arg
+elif [[ $1 == -r || $1 == -s ]]; then
+	mcVer=$(getLatestVer $1)
+
+# Version arg
+elif [[ $(curl https://launchermeta.mojang.com/mc/game/version_manifest.json -L) == *$1*  ]]; then
+	mcVer=$1
+
+# Version not found
+else
+	echo "Version $@ does not exist"
+        echo
+        sendHelp 1
 fi
+
+meta=$(getVerMeta $mcVer)
+downloadClient $meta
+downloadServer $meta
